054d40bbae3dea33dd0d984493dc0c1884af68ca
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-03-14 12:39:43 -0400

    made a concatenate

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,6 +1,10 @@
-window.alert("Hello, World!")
-document.write("Hello, World!")
+window.alert("Hello, World!");
+document.write("Hello, World!");
 var A = " This is a string!";
 document.write(A);
-document.write("Lisa told Bart, \"knock it off, Bart! or I'll tell dad!\"<br>
-\"Eat my shorts!\"Bart responded.");
+document.write("<br>\"");
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\"Bart responded.");
+document.write("<br>\"");
+document.write("\Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
