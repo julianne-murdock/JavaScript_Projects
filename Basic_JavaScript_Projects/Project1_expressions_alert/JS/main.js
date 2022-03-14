793402e89d5c606b9c62cf2a430bb0820a20fd59
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-03-14 12:26:09 -0400

    continuing js project

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -2,3 +2,5 @@ window.alert("Hello, World!")
 document.write("Hello, World!")
 var A = " This is a string!";
 document.write(A);
+document.write("Lisa told Bart, \"knock it off, Bart! or I'll tell dad!\"<br>
+\"Eat my shorts!\"Bart responded.");
