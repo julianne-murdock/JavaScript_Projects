4bc9c7b23d6cbc7e4d14a0335d72cf933b55262e
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-03-14 13:53:13 -0400

    learned how to chane font color in java

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,10 +1,21 @@
-window.alert("Hello, World!");
+window.alert("Hello, World!"); //saying my first hello world
 document.write("Hello, World!");
 var A = " This is a string!";
 document.write(A);
-document.write("<br>\"");
-document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\"Bart responded.");
-document.write("<br>\"");
-document.write("\Be who you are and say what you feel,"
+document.write("<br>");
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\"Bart responded."); //working on commas
+document.write("<br>");
+document.write("\Be who you are and say what you feel," //concatenated strings
 + " because those who mind don\'t matter and those who matter don\'t mind.\""
 + "-Dr. Seuss");
+document.write("<br>");
+var B = " Concatenated" + " String"
+document.write(B);
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "hermoine", Daughter = "Penny", Son = "Zorro";
+document.write("<br>");
+document.write(Dad); // calling a variable out of multiple
+document.write("<br>");
+document.write(3+3);
+document.write("<br>");
+var str = "This text is blue";
+document.write(str.fontcolor( "blue" ));
