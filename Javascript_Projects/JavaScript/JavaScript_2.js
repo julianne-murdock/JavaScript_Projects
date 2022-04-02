ee4d981a6fc9eb81275e57d287787b46fc543901
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-04-02 16:26:50 -0400

    updated

--- Javascript_Projects/JavaScript/JavaScript_2.js
+++ Javascript_Projects/JavaScript/JavaScript_2.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function myFunction1() {
   // Get the value of the input field with id="numb"
   let x = document.getElementById("numb").value;
   // If x is Not a Number or less than one or greater than 10
